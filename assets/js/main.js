d9e8906ae30363373ef8815ca8f662e0afe9df55
Andrew Akagawa <andrewakagawa@gmail.com>
2019-07-13 09:59:04 -0700

    adding preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 // This file has the ghost-bootstrap theme javascript
 $(document).ready(function() {
+
+    // remove preloader
+    $('.preloader').fadeOut('slow');
+    
     // load tooltips
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
